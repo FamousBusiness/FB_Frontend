2e486628c9a3190dfdef934f19b417e61b761125
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2024-10-08 22:57:51 +0530

    nc

--- app/plan/components/AutoPay/qrCode.js
+++ app/plan/components/AutoPay/qrCode.js
@@ -23,7 +23,7 @@ export const GenerateQRCode = ({value})=> {
 
     return (
          <div style={{ width: '100%', height: '100%' }}>
-            <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
+            <canvas ref={canvasRef} />
         </div>
     );
 };
