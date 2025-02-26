5294ed1a127276eaca7ed8aa2e2b0522a8c28ba7
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2025-02-26 21:47:35 +0530

    Premium Plan Payment issue fix

--- app/plan/components/Plan.js
+++ app/plan/components/Plan.js
@@ -225,19 +225,26 @@ function Plan() {
                                         <Badge.Ribbon text={`${item.plan.tag_line}`} color='blue'>
                                             <Card style={{ boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px' }} bordered={false} className=' shadow-xl relative'>
                                                 <Row align='top' gutter={[12, 24]}>
+
                                                     <Col span={5}>
                                                         {item.plan.name === 'Starter' && <Image src='/plans/rocket.svg' alt='icon' width={50} height={50} />}
+
                                                         {item.plan.name === 'Business' && <Image src='/plans/crown (1).svg' alt='icon' width={50} height={50} />}
+
                                                         {item.plan.name === 'Enterprises' && <Image src='/plans/business-award.svg' alt='icon' width={50} height={50} />}
+
                                                         {item.plan.name === 'Gold' && <Image src='/plans/award.svg' alt='icon' width={50} height={50} />}
                                                     </Col>
+
                                                     <Col span={19}>
                                                         <p className=' text-lg font-semibold'>{item.plan.name}</p>
                                                         {/* <p className=' text-lg text-blue-500 mt-2'>{item.plan.tag_line}</p> */}
                                                     </Col>
+
                                                     <Col span={24}>
                                                         <div className=' h-2 bg-gradient-to-tr rounded-full from-orange-500 to-purple-400 ' />
                                                     </Col>
+
                                                     {/* Plan price  */}
                                                     <Col span={24}>
                                                         <Flex vertical gap={4}>
@@ -246,48 +253,62 @@ function Plan() {
                                                             <Typography.Text color='green' italic>Included 18% GST Invoice Available</Typography.Text>
                                                         </Flex>
                                                     </Col>
+
                                                     {/* plan Feature  */}
                                                     {/* {item.plan.name==='Starter' && <div className=' h-6'></div>} */}
                                                     <Col span={24}>
                                                         <div className=' flex flex-col justify-start space-y-1 text-base text-black '>
+
                                                             {item.plan.verified ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.verified}<MdVerified className=' text-indigo-600' /></Flex> :
                                                                 <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Verified Business Icon</Text>
                                                                     <MdVerified className=' text-indigo-600' /></Flex>}
+
                                                             {item.plan.trusted ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.trusted}
                                                                 <div style={{ fontSize: "8px" }} className=' px-1 rounded-sm flex items-center justify-center h-4  text-white font-bold text-center bg-[linear-gradient(to_right,theme(colors.red.600),theme(colors.red.200),theme(colors.green.600),theme(colors.fuchsia.600),theme(colors.green.600),theme(colors.red.200),theme(colors.red.600))] bg-[length:200%_auto] animate-gradient'>
                                                                     Trusted
                                                                 </div>
+                                                                
                                                             </Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Trusted Tag</Text>
                                                                 <div style={{ fontSize: "8px" }} className=' px-1 rounded-sm flex items-center justify-center h-4  text-white font-bold text-center bg-[linear-gradient(to_right,theme(colors.red.600),theme(colors.red.200),theme(colors.green.600),theme(colors.fuchsia.600),theme(colors.green.600),theme(colors.red.200),theme(colors.red.600))] bg-[length:200%_auto] animate-gradient'>
                                                                     Trusted
                                                                 </div>
                                                             </Flex>}
+
                                                             {item.plan.trending ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.trending}
                                                                 <div style={{ fontSize: '8px' }} className=' px-1 rounded-sm flex items-center justify-center h-4 text-white font-bold text-center bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient'>
                                                                     Trending
                                                                 </div>
+
                                                             </Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Trending Tag</Text>
                                                                 <div style={{ fontSize: '8px' }} className=' px-1 rounded-sm flex items-center justify-center h-4 text-white font-bold text-center bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] animate-gradient'>
                                                                     Trending
                                                                 </div>
                                                             </Flex>}
+
                                                             {item.plan.authorized ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.authorized}<FaUser className=' text-green-500' /></Flex> :
                                                                 <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>   No Leads</Text><FaUser className=' text-green-500' /></Flex>}
+
                                                             {item.plan.sponsor ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.sponsor}<FcBriefcase /></Flex>
                                                                 : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  No Job Post</Text><FcBriefcase /></Flex>}
+
                                                             {item.plan.super ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.super}</Flex>
                                                                 : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Authorized Dealer Tag</Text> </Flex>}
+
                                                             {item.plan.premium ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.premium}<MdWorkspacePremium className=' text-amber-600' /></Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Premium Seller Tag</Text><MdWorkspacePremium className=' text-amber-600' /></Flex>}
+
                                                             {item.plan.industry_leader ? <Flex Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.industry_leader}</Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Industry Leader Tag</Text></Flex>}
+
                                                             {item.plan.extra_benefits ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.extra_benefits}</Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-orange-500' /><Text disabled>Sponser Tag</Text></Flex>}
 
                                                             {item.plan.extra_benefits1 ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.extra_benefits1}</Flex>
                                                                 : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Super Seller Tag</Text></Flex>}
                                                         </div>
                                                     </Col>
+
                                                     <Col span={24}>
-                                                        <HandlePayment id={item.plan.id} amount={item.plan.price} />
+                                                        <HandlePayment id={item.id} amount={item.plan.price} />
                                                     </Col>
+
                                                     {/* <Col span={24}>
                                                         <Flex justify='center' align='center'><Text mark>Daily Spend only ₹ {((item.plan.price) / 365).toFixed()}</Text> </Flex>
                                                     </Col> */}
@@ -356,14 +377,20 @@ function Plan() {
                                                                 Trending
                                                             </div>
                                                         </Flex>}
+
                                                         {item.plan.authorized ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.authorized}<FaUser className=' text-green-500' /></Flex> :
                                                             <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>   No Leads</Text><FaUser className=' text-green-500' /></Flex>}
+
                                                         {item.plan.sponsor ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.sponsor}<FcBriefcase /></Flex>
                                                             : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  No Job Post</Text><FcBriefcase /></Flex>}
+
                                                         {item.plan.super ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.super}</Flex>
                                                             : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Authorized Dealer Tag</Text> </Flex>}
+
                                                         {item.plan.premium ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.premium}<MdWorkspacePremium className=' text-amber-600' /></Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Premium Seller Tag</Text><MdWorkspacePremium className=' text-amber-600' /></Flex>}
+
                                                         {item.plan.industry_leader ? <Flex Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.industry_leader}</Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-green-500' /><Text disabled>  Industry Leader Tag</Text></Flex>}
+
                                                         {item.plan.extra_benefits ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.extra_benefits}</Flex> : <Flex gap={4} align='center'><FaCircleXmark className=' text-red-500 drop-shadow-lg shadow-orange-500' /><Text disabled>Sponser Tag</Text></Flex>}
 
                                                         {item.plan.extra_benefits1 ? <Flex gap={4} align='center'><FaCircleCheck className=' text-green-500 drop-shadow-lg shadow-green-500' />{item.plan.extra_benefits1}</Flex>
@@ -373,7 +400,7 @@ function Plan() {
                                                 
                                                 <Col span={24}>
                                                     {/* <TrialPlan premium={item.id} /> */}
-                                                    <HandlePayment id={item.plan.id} amount={item.plan.price} />
+                                                    <HandlePayment id={item.id} amount={item.plan.price} />
                                                 </Col>
                                                 {/* <Col span={24}>
                                                         <Flex justify='center' align='center'><Text mark>Daily Spend only ₹ {((item.plan.price) / 365).toFixed()}</Text> </Flex>
