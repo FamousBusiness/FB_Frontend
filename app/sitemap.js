03f3c7883a6d30e4de7f58af03b022df5b82bfb3
RUNRanjit <88085245+RUNRanjit@users.noreply.github.com>
2025-03-23 09:31:38 +0530

    meta tag and schema added

---
+++ app/sitemap.js
@@ -0,0 +1,14 @@
+
+
+
+export default function sitemap() {
+    return [
+      {
+        url: 'https://famousbusiness.in/',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      }
+    ]
+}
+
